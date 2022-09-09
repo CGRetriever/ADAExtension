59bc6afa85e749343b0cade80486cc9bf81a7107
CGRetriever <rannigro@gmail.com>
2022-09-09 02:26:57 -0400

    Update content.js

--- content.js
+++ content.js
@@ -14,14 +14,5 @@ for (elt of paragraphs){
       }
 }
 
-/*
-This grabs all of the static imgs that load and turn them into whichever image I put
-down there
-*/
-var imgs = document.getElementsByTagName("img");
-for(var i=0, l=imgs.length; i<l; i++) {
-imgs[i].src = "https://www.usnews.com/dims4/USNEWS/8ad72b6/2147483647/thumbnail/640x420/quality/85/?url=http%3A%2F%2Fmedia.beam.usnews.com%2F3c%2Fe4d17089c08f5056304921b4cdda90%2Fmedia%3Ac76e88a024eb4d2eaf9640f6d01916deBiden_Inauguration_19990.jpg";
-}
-
 
 
